c1f097fa6f441da64f58ba568878a5ba329802f0
Ned Collyer <ned.collyer@gmail.com>
2025-01-18 00:07:02 +1100

    feat: background render color

--- src/main.zig
+++ src/main.zig
@@ -16,6 +16,8 @@ pub fn main() !void {
     var window: ?*c.SDL_Window = undefined;
     var renderer: ?*c.SDL_Renderer = undefined;
 
+    // https://github.com/ziglang/zig/issues/22494
+    // c.SDL_WINDOW_VULKAN
     const SDL_WINDOW_VULKAN = 0x0000000010000000;
 
     if (!c.SDL_CreateWindowAndRenderer("My Game window", 640, 480, SDL_WINDOW_VULKAN, &window, &renderer)) {
@@ -40,8 +42,10 @@ pub fn main() !void {
         }
 
         _ = c.SDL_RenderClear(renderer);
+        _ = c.SDL_SetRenderDrawColor(renderer, 42, 69, 0, 0);
         _ = c.SDL_RenderPresent(renderer);
+        _ = c.SDL_SetRenderVSync(renderer, c.SDL_RENDERER_VSYNC_ADAPTIVE);
 
-        c.SDL_Delay(10);
+        c.SDL_Delay(1);
     }
 }
